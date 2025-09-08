53621af0ccd52647439e3e8401e830edf9defdaf
Anthony Arcuri <arcuriaj@mail.uc.edu>
2025-09-07 21:14:04 -0400

    working webpage

---
+++ src/test/java/com/arcuriaj/firstspringboot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcuriaj.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
